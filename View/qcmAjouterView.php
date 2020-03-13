6b1d8597f091a18f6663967abf66c545877fab8e
Dina <dina.bel1999@gmail.com>
2020-03-13 09:39:25 +0100

    ajout de l'appel à la fonction input de la class Form

--- View/qcmAjouterView.php
+++ View/qcmAjouterView.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    
+
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" type="text/css" href="form.css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
@@ -14,46 +15,46 @@
 
 
 <body>
-    
-<?php
-spl_autoload_register(function ($class_name)
-{
-    include_once "../class/".$class_name."class.php";
-});
-?>
 
-<div class="privew">
-<form action="" method="post">
-    <div class="questionsBox">
-        <div class="questions"><?php echo $form->input('question'); ?></div>
-        <ul class="answerList">
-            <li>
-                <label>
-                    <?php echo "reponse 1 : ".$form->input('reponse1'); ?>
-                </label>
-            </li>
-            <li>
-                <label>
-                    <?php echo "reponse 2 : ".$form->input('reponse2'); ?>
-                </label>
-            </li>
-            <li>
-                <label>
-                    <?php echo "reponse 3 : ".$form->input('reponse3'); ?>
-                </label>
-            </li>
-            <li>
-                <label>
-                    <?php echo "reponse 4 : ".$form->input('reponse4');;  ?>
-                </label>
-            </li>
-            <?php echo $form->submit(); ?>
-        </ul>
-        
+    <?php
+    spl_autoload_register(function ($class_name) {
+        include_once "../class/" . $class_name . "class.php";
+    });
+    $form = new Form($_POST);
+    ?>
+
+    <div class="privew">
+        <form action="" method="post">
+            <div class="questionsBox">
+                <div class="questions"><?php echo $form->input('question'); ?></div>
+                <ul class="answerList">
+                    <li>
+                        <label>
+                            <?php echo "reponse 1 : " . $form->input('reponse1'); ?>
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <?php echo "reponse 2 : " . $form->input('reponse2'); ?>
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <?php echo "reponse 3 : " . $form->input('reponse3'); ?>
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <?php echo "reponse 4 : " . $form->input('reponse4');;  ?>
+                        </label>
+                    </li>
+                    <?php echo $form->submit(); ?>
+                </ul>
+
+            </div>
+        </form>
     </div>
-</form>
-</div>
 
 </body>
-</html>
 
+</html>
