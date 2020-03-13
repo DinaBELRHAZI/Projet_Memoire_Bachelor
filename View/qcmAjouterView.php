47691560c76eab31b4cb9f0287238f27ad050d18
Dina <dina.bel1999@gmail.com>
2020-03-13 09:42:53 +0100

    mise en lien avec le form.css  Please enter the commit message for your changes. Lines starting

--- View/qcmAjouterView.php
+++ View/qcmAjouterView.php
@@ -8,7 +8,7 @@
     <title>Document</title>
 
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link rel="stylesheet" type="text/css" href="form.css">
+    <link rel="stylesheet" type="text/css" href="../css/form.css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
